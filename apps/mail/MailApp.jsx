f9a7387711a45ad9afbd5d307735a33ee49d09a5
litalg8 <litalgottlieb@gmail.com>
2020-08-26 17:21:40 +0300

    no lala

--- apps/mail/MailApp.jsx
+++ apps/mail/MailApp.jsx
@@ -5,7 +5,7 @@ export class MailApp extends React.Component {
             <section>
                 <h2>You've Got Mail</h2>
                 <h2>You've Got SheMail</h2>
-                <h2>You've Got LALALALALALALAL</h2>
+          
             </section>
         )
     }
